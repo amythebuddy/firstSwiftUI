2966b689bd5c3446b78ee78a703c9e71e8e7b838
StudentAM <studentam@Administrators-MacBook-Pro.local>
2024-02-26 09:55:23 -0800

    add

--- firstSwiftUI/firstSwiftUI/ContentView.swift
+++ firstSwiftUI/firstSwiftUI/ContentView.swift
@@ -8,21 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var count = 0
+    @State private var count = 0 // declare and initialize count
     var body: some View {
         VStack {
             Text("Click buttons below to change the counter")
-            Text("\(count)")
+            Text("\(count)") // show the counter
                 .padding(.vertical, 30)
             HStack {
-                Button("Increment", action: increaseCounter)
-                    .padding()
+                Button("Increment", action: increaseCounter) //point to the function
+                    .padding(10)
                     .background(Color.blue)
                     .foregroundColor(.white)
                     .cornerRadius(10)
                 
-                Button("Decrement", action: decreaseCounter)
-                    .padding()
+                Button("Decrement", action: decreaseCounter) // point to the function
+                    .padding(10)
                     .background(Color.blue)
                     .foregroundColor(.white)
                     .cornerRadius(10)
@@ -32,12 +32,14 @@ struct ContentView: View {
     }
     func increaseCounter(){
         count += 1
+        //if count is larger than 150 then count stops
         if count > 150 {
             count = 150
         }
     }
     func decreaseCounter(){
         count -= 1
+        //if count is smaller than 0 then count stops
         if count < 0 {
             count = 0
         }
