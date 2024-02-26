dab182e52a95804dd2438e743dcbbd3c8a7de7fd
StudentAM <studentam@Administrators-MacBook-Pro.local>
2024-02-26 09:59:44 -0800

    add

--- firstSwiftUI/firstSwiftUI/ContentView.swift
+++ firstSwiftUI/firstSwiftUI/ContentView.swift
@@ -13,32 +13,32 @@ struct ContentView: View {
         VStack {
             Text("Click buttons below to change the counter")
             Text("\(count)") // show the counter
-                .padding(.vertical, 30)
+                .padding(.vertical, 30) // create vertical space between the button and the text
             HStack {
                 Button("Increment", action: increaseCounter) //point to the function
-                    .padding(10)
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
+                    .padding(10) // create space between the text and border
+                    .background(Color.blue) // create blue background
+                    .foregroundColor(.white) // white text
+                    .cornerRadius(10) // round the corner of the box
                 
                 Button("Decrement", action: decreaseCounter) // point to the function
-                    .padding(10)
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
+                    .padding(10) // create space between the text and border
+                    .background(Color.blue) // create blue background
+                    .foregroundColor(.white) // white text
+                    .cornerRadius(10) // round the corner of the box
             }
         }
         .padding()
     }
     func increaseCounter(){
-        count += 1
-        //if count is larger than 150 then count stops
-        if count > 150 {
-            count = 150
+        count += 1 // count increases by 1
+        //if count is larger than 50 then count stops
+        if count > 50 {
+            count = 50
         }
     }
     func decreaseCounter(){
-        count -= 1
+        count -= 1 // count decreases by 1
         //if count is smaller than 0 then count stops
         if count < 0 {
             count = 0
